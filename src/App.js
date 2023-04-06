91173342c5045e717350e0cfa9440c4143702366
Marly Diallo <marly.diallo2021@gmail.com>
2023-04-06 11:06:07 +0200

    iteration1-2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import contacts from "./contacts.json";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const { contactList, setContactList } = useState(contacts);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h2>IronContacts</h2>
+        <table>
+          <tr>
+            <td>
+              <h3>Picture</h3>
+            </td>
+            <td>
+              <h3>Name</h3>
+            </td>
+            <td>
+              <h3>Popularity</h3>
+            </td>
+            <td>
+              <h3>Won Oscar</h3>
+            </td>
+            <td>
+              <h3>Won Emmy</h3>
+            </td>
+          </tr>
+          {contacts
+            .map((contact) => {
+              return (
+                <tr key={contact.id} className="contact">
+                  <td>
+                    <img
+                      src={contact.pictureUrl}
+                      alt="contact"
+                      height="80px"
+                      width="60ps"
+                    ></img>
+                  </td>
+                  <td>
+                    <p>{contact.name}</p>
+                  </td>
+                  <td>
+                    <p>{contact.popularity}</p>
+                  </td>
+                  <td>
+                    <p>{contact.wonOscar ? "Tropy" : ""}</p>
+                  </td>
+                  <td>
+                    <p>{contact.wonEmmy ? "Tropy" : ""}</p>
+                  </td>
+                </tr>
+              );
+            })
+            .slice(0, 5)}
+        </table>
+      </div>
     </div>
   );
 }
-
 export default App;
